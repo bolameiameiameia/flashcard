bb41786c4a517735ed1dcdcf51da6de1fd591ee3
bola666 <163007758+bolameiameiameia@users.noreply.github.com>
2024-10-07 11:14:53 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Brasil' ,
+    'Quem descobriu o Brasil' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Título' ,
+    'Pergunta' ,
+    'Resposta'
+)
+
